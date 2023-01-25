f80d5aff4c7ff952ee35ba5bb57eb2db3d7dc8df
mac <u6533386@anu.edu.au>
2023-01-26 06:28:15 +1100

    Route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/days', [\App\Http\Controllers\DateTimeController::class, 'days']);
+    Route::post('/weeks', [\App\Http\Controllers\DateTimeController::class, 'weeks']);
 });
